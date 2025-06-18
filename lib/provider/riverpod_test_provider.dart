3036f604255577bf12c5294a3ce0c6d4f0a5460d
Min Khant Kyaw <kyawkhant2021@gmail.com>
2025-06-18 11:14:02 +0630

    State Notifier Provider Testing Success

--- lib/provider/riverpod_test_provider.dart
+++ lib/provider/riverpod_test_provider.dart
@@ -43,14 +43,31 @@ class AppState {
   }
 }
 
-final searchProvider = StateNotifierProvider<SerchNotifier, String>((ref) {
+final searchProvider = StateNotifierProvider<SerchNotifier, SearchState>((ref) {
   return SerchNotifier();
 });
 
-class SerchNotifier extends StateNotifier<String> {
-  SerchNotifier() : super('');
+class SerchNotifier extends StateNotifier<SearchState> {
+  SerchNotifier() : super(SearchState(search: '', isChange: false));
 
   void search(String query) {
-    state = query;
+    state = state.copyWith(search: query);
+  }
+
+  void onChange(bool isChange) {
+    state = state.copyWith(isChange: isChange);
+  }
+}
+
+class SearchState {
+  final String search;
+  final bool isChange;
+  SearchState({required this.search, required this.isChange});
+
+  SearchState copyWith({String? search, bool? isChange}) {
+    return SearchState(
+      search: search ?? this.search,
+      isChange: isChange ?? this.isChange,
+    );
   }
 }
--- lib/screens/riverpod_test_screen.dart
+++ lib/screens/riverpod_test_screen.dart
@@ -10,102 +10,138 @@ class RiverpodTestScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     // var value = ref.watch(counterStateProvider);
+
     print("value");
     return Scaffold(
       appBar: AppBar(title: Text("RiverPods testing")),
       body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            // Text(
-            //   value.toString(),
-            //   style: Theme.of(context).textTheme.titleLarge,
-            // ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  onPressed: () {
-                    ref.read(counterStateProvider.state).state++;
-                  },
-                  child: Icon(Icons.plus_one),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    ref.read(counterStateProvider.state).state--;
-                  },
-                  child: Icon(Icons.exposure_minus_1_outlined),
-                ),
-              ],
+            TextField(
+              onChanged: (value) {
+                ref.read(searchProvider.notifier).search(value);
+              },
             ),
-            SizedBox(height: 30),
             Consumer(
               builder: (context, ref, child) {
-                final value = ref.watch(counterStateProvider);
-                final jokeAsync = ref.watch(getApi);
-                final slider = ref.watch(sliderProvider);
-                return Center(
-                  child: Column(
-                    children: [
-                      Text(value.toString()),
-                      ElevatedButton(
-                        onPressed: () {
-                          ref.read(counterStateProvider.state).state++;
-                        },
-                        child: Icon(Icons.add),
-                      ),
-                      jokeAsync.when(
-                        data:
-                            (joke) => Column(
-                              children: [
-                                Text(joke.type),
-                                Text(joke.setup),
-                                Text(joke.punchline),
-                              ],
-                            ),
-                        error: (e, st) => Text("error $e"),
-                        loading: () => CircularProgressIndicator(),
-                      ),
-                      InkWell(
-                        onTap: () {
-                          final statePassword = ref.read(
-                            sliderProvider.notifier,
-                          );
-                          statePassword.state = statePassword.state.copyWith(
-                            showPassword: !slider.showPassword,
-                          );
-                        },
-                        child: Container(
-                          width: 200,
-                          height: 200,
-                          child:
-                              slider.showPassword
-                                  ? Icon(Icons.remove_red_eye)
-                                  : Icon(Icons.image),
-                        ),
-                      ),
-                      Container(
-                        height: 200,
-                        width: 200,
-                        color: Colors.red.withOpacity(slider.slider),
-                      ),
-                      Slider(
-                        value: slider.slider,
-                        onChanged: (value) {
-                          final stateProvider = ref.read(
-                            sliderProvider.notifier,
-                          );
-                          stateProvider.state = stateProvider.state.copyWith(
-                            slider: value,
-                          );
-                        },
-                      ),
-                    ],
-                  ),
+                final search = ref.watch(
+                  searchProvider.select((state) => state.search),
+                );
+                print("one");
+                return Text("The result" + search);
+              },
+            ),
+            Consumer(
+              builder: (context, ref, child) {
+                final change = ref.watch(
+                  searchProvider.select((state) => state.isChange),
+                );
+                return Switch(
+                  value: change,
+                  onChanged: (value) {
+                    ref.read(searchProvider.notifier).onChange(value);
+                  },
                 );
               },
             ),
           ],
         ),
+        /////////////////////
+        ///State Provider Testing
+        ////////////////////
+        // child: Column(
+        //   children: [
+        //     // Text(
+        //     //   value.toString(),
+        //     //   style: Theme.of(context).textTheme.titleLarge,
+        //     // ),
+        //     Row(
+        //       mainAxisAlignment: MainAxisAlignment.center,
+        //       children: [
+        //         ElevatedButton(
+        //           onPressed: () {
+        //             ref.read(counterStateProvider.state).state++;
+        //           },
+        //           child: Icon(Icons.plus_one),
+        //         ),
+        //         ElevatedButton(
+        //           onPressed: () {
+        //             ref.read(counterStateProvider.state).state--;
+        //           },
+        //           child: Icon(Icons.exposure_minus_1_outlined),
+        //         ),
+        //       ],
+        //     ),
+        //     SizedBox(height: 30),
+        //     Consumer(
+        //       builder: (context, ref, child) {
+        //         final value = ref.watch(counterStateProvider);
+        //         final jokeAsync = ref.watch(getApi);
+        //         final slider = ref.watch(sliderProvider);
+        //         return Center(
+        //           child: Column(
+        //             children: [
+        //               Text(value.toString()),
+        //               ElevatedButton(
+        //                 onPressed: () {
+        //                   ref.read(counterStateProvider.state).state++;
+        //                 },
+        //                 child: Icon(Icons.add),
+        //               ),
+        //               jokeAsync.when(
+        //                 data:
+        //                     (joke) => Column(
+        //                       children: [
+        //                         Text(joke.type),
+        //                         Text(joke.setup),
+        //                         Text(joke.punchline),
+        //                       ],
+        //                     ),
+        //                 error: (e, st) => Text("error $e"),
+        //                 loading: () => CircularProgressIndicator(),
+        //               ),
+        //               InkWell(
+        //                 onTap: () {
+        //                   final statePassword = ref.read(
+        //                     sliderProvider.notifier,
+        //                   );
+        //                   statePassword.state = statePassword.state.copyWith(
+        //                     showPassword: !slider.showPassword,
+        //                   );
+        //                 },
+        //                 child: Container(
+        //                   width: 200,
+        //                   height: 200,
+        //                   child:
+        //                       slider.showPassword
+        //                           ? Icon(Icons.remove_red_eye)
+        //                           : Icon(Icons.image),
+        //                 ),
+        //               ),
+        //               Container(
+        //                 height: 200,
+        //                 width: 200,
+        //                 color: Colors.red.withOpacity(slider.slider),
+        //               ),
+        //               Slider(
+        //                 value: slider.slider,
+        //                 onChanged: (value) {
+        //                   final stateProvider = ref.read(
+        //                     sliderProvider.notifier,
+        //                   );
+        //                   stateProvider.state = stateProvider.state.copyWith(
+        //                     slider: value,
+        //                   );
+        //                 },
+        //               ),
+        //             ],
+        //           ),
+        //         );
+        //       },
+        //     ),
+        //   ],
+        // ),
       ),
     );
   }
